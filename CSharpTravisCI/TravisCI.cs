c3b19aa35863e46c5e0e758bfc147d96e6514d9f
bradyxiao <bradyxiao@tencent.com>
2019-01-22 16:44:45 +0800

    update

---
+++ CSharpTravisCI/TravisCI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpTravisCI
+{
+    public class TravisCI
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ CSharpTravisCITests/TravisCITests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using CSharpTravisCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpTravisCI.Tests
+{
+    [TestFixture()]
+    public class TravisCITests
+    {
+        [Test()]
+        public void AddTest()
+        {
+            TravisCI travisCI = new TravisCI();
+            Assert.AreEqual(4, travisCI.Add(1, 3));
+            Console.WriteLine("RESULT " + 4);
+        }
+
+    }
+}
+
+
+/* ============================================================================== 
+* Copyright 2016-2019 Tencent Cloud. All Rights Reserved.
+* Auth：bradyxiao 
+* Date：2019/1/22 15:57:35 
+* ==============================================================================*/
